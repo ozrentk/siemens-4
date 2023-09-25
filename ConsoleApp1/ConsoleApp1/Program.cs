240e5381dfe86a2988e01e1272cb33bdac14135c
Ozren TK <ozren.krznaric@gmail.com>
2023-09-25 15:34:48 +0200

    Programčić

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -9,10 +9,20 @@ namespace ConsoleApp1
 {
     internal class Program
     {
+        /// <summary>
+        /// Ovo je programčić
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             ConfigurationService.Configure("URL", "https://www.google.com");
             ConfigurationService.Configure("ConnectionString", "Server=.;Database=myDataBase;User=[USERNAME-HERE];Password=[PASSWORD-HERE]");
+            ConfigurationService.Configure("URL2", "https://github.com/");
+            ConfigurationService.Configure("URL2", "https://github.com/");
+            ConfigurationService.Configure("URL2", "https://github.com/");
+            ConfigurationService.Configure("URL2", "https://github.com/");
+            ConfigurationService.Configure("URL2", "https://github.com/");
+            ConfigurationService.Configure("URL2", "https://github.com/");
             ConfigurationService.Configure("URL2", "https://github.com/");
             Console.ReadLine();
         }
