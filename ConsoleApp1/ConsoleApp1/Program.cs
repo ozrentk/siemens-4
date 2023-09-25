8ed6d96229a211748f058b8b4e98c2c153601e6c
Ozren TK <ozren.krznaric@gmail.com>
2023-09-25 15:38:14 +0200

    xxyyzz

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -18,12 +18,7 @@ namespace ConsoleApp1
             ConfigurationService.Configure("URL", "https://www.google.com");
             ConfigurationService.Configure("ConnectionString", "Server=.;Database=myDataBase;User=[USERNAME-HERE];Password=[PASSWORD-HERE]");
             ConfigurationService.Configure("URL2", "https://github.com/");
-            ConfigurationService.Configure("URL2", "https://github.com/");
-            ConfigurationService.Configure("URL2", "https://github.com/");
-            ConfigurationService.Configure("URL2", "https://github.com/");
-            ConfigurationService.Configure("URL2", "https://github.com/");
-            ConfigurationService.Configure("URL2", "https://github.com/");
-            ConfigurationService.Configure("URL2", "https://github.com/");
+            ConfigurationService.Configure("URL3", "https://github.com/");
             Console.ReadLine();
         }
     }
