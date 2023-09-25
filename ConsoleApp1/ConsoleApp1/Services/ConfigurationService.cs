a3910015f2e470b6893eca158df6c2abbfb3f9ab
Ozren TK <ozren.krznaric@gmail.com>
2023-09-25 14:40:18 +0200

    Added configuration service

---
+++ ConsoleApp1/ConsoleApp1/Services/ConfigurationService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Services
+{
+    public static class ConfigurationService
+    {
+        public static void Configure(string name, string value)
+        {
+            Console.WriteLine($"Config: {name}={value}");
+        }
+    }
+}
